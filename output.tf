453196e2931241f5fb35ad5a87946980158c17a5
kaushik <96657705+kaushikbl@users.noreply.github.com>
2025-07-28 21:07:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.eks.id
+  value = aws_eks_cluster.my_eks.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.eks.id
+  value = aws_eks_node_group.my_eks.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.eks_vpc.id
+  value = aws_vpc.my_eks_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.eks_subnet[*].id
+  value = aws_subnet.my_eks_subnet[*].id
 }
