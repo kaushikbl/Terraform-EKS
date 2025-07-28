59a4c301f599b619a37742e691582da003d3d378
kaushik <96657705+kaushikbl@users.noreply.github.com>
2025-07-28 20:42:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,36 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "eks_vpc" {
+resource "aws_vpc" "my_eks_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "eks-vpc"
+    Name = "my-eks-vpc"
   }
 }
 
-resource "aws_subnet" "eks_subnet" {
+resource "aws_subnet" "my_eks_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.my_eks_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.my_eks_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "eks-subnet-${count.index}"
+    Name = "my-eks-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_internet_gateway" "my_eks_igw" {
+  vpc_id = aws_vpc.my_eks_vpc.id
 
   tags = {
-    Name = "eks-igw"
+    Name = "my-eks-igw"
   }
 }
 
-resource "aws_route_table" "eks_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_route_table" "my_eks_route_table" {
+  vpc_id = aws_vpc.my_eks_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -39,18 +39,18 @@ resource "aws_route_table" "eks_route_table" {
   }
 
   tags = {
-    Name = "eks-route-table"
+    Name = "my-eks-route-table"
   }
 }
 
-resource "aws_route_table_association" "eks_association" {
+resource "aws_route_table_association" "my_eks_association" {
   count          = 2
-  subnet_id      = aws_subnet.eks_subnet[count.index].id
-  route_table_id = aws_route_table.eks_route_table.id
+  subnet_id      = aws_subnet.my_eks_subnet[count.index].id
+  route_table_id = aws_route_table.my_eks_route_table.id
 }
 
-resource "aws_security_group" "eks_cluster_sg" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_security_group" "my_eks_cluster_sg" {
+  vpc_id = aws_vpc.my_eks_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "eks_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "my-eks-cluster-sg"
   }
 }
 
-resource "aws_security_group" "eks_node_sg" {
-  vpc_id = aws_vpc.eks_vpc.id
+resource "aws_security_group" "my_eks_node_sg" {
+  vpc_id = aws_vpc.my_eks_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,17 +82,17 @@ resource "aws_security_group" "eks_node_sg" {
   }
 
   tags = {
-    Name = "eks-node-sg"
+    Name = "my-eks-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = "myeks-cluster"
-  role_arn = aws_iam_role.myeks_cluster_role.arn
+resource "aws_eks_cluster" "my_eks" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.my_eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.eks_subnet[*].id
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    subnet_ids         = aws_subnet.my_eks_subnet[*].id
+    security_group_ids = [aws_security_group.my_eks_cluster_sg.id]
   }
 }
 
@@ -106,11 +106,11 @@ resource "aws_eks_addon" "ebs_csi_driver" {
 }
 
 
-resource "aws_eks_node_group" "eks" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+resource "aws_eks_node_group" "my_eks" {
+  cluster_name    = aws_eks_cluster.my_eks.name
+  node_group_name = "my-eks-node-group"
+  node_role_arn   = aws_iam_role.my_eks_node_group_role.arn
+  subnet_ids      = aws_subnet.my_eks_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -122,12 +122,12 @@ resource "aws_eks_node_group" "eks" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.eks_node_sg.id]
+    source_security_group_ids = [aws_security_group.my_eks_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "myeks_cluster_role" {
-  name = "myeks-cluster-role"
+resource "aws_iam_role" "my_eks_cluster_role" {
+  name = "my_eks-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -145,13 +145,13 @@ resource "aws_iam_role" "myeks_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "myeks_cluster_role_policy" {
-  role       = aws_iam_role.myeks_cluster_role.name
+resource "aws_iam_role_policy_attachment" "my_eks_cluster_role_policy" {
+  role       = aws_iam_role.my_eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "myeks_node_group_role" {
-  name = "myeks-node-group-role"
+resource "aws_iam_role" "my_eks_node_group_role" {
+  name = "my-eks-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -169,22 +169,22 @@ resource "aws_iam_role" "myeks_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "myeks_node_group_role_policy" {
-  role       = aws_iam_role.myeks_node_group_role.name
+resource "aws_iam_role_policy_attachment" "my_eks_node_group_role_policy" {
+  role       = aws_iam_role.my_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "myeks_node_group_cni_policy" {
-  role       = aws_iam_role.myeks_node_group_role.name
+resource "aws_iam_role_policy_attachment" "my_eks_node_group_cni_policy" {
+  role       = aws_iam_role.my_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "myeks_node_group_registry_policy" {
-  role       = aws_iam_role.myeks_node_group_role.name
+resource "aws_iam_role_policy_attachment" "my_eks_node_group_registry_policy" {
+  role       = aws_iam_role.my_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "myeks_node_group_ebs_policy" {
-  role       = aws_iam_role.myeks_node_group_role.name
+resource "aws_iam_role_policy_attachment" "my_eks_node_group_ebs_policy" {
+  role       = aws_iam_role.my_eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
